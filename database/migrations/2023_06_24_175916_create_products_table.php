17686688606dad39b607d3da8739ed5a46e59485
kodaikumatani <koudai.aco5423@gmail.com>
2023-06-25 03:06:29 +0900

    DBテーブルの作成

--- database/migrations/2023_06_24_175916_create_products_table.php
+++ database/migrations/2023_06_24_175916_create_products_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->unsignedInteger('producer_code');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->onDelete('cascade');
+            $table->string('name', 100);
+            $table->unsignedInteger('price');
+            $table->unique(['user_id', 'name', 'price']);
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
--- database/migrations/2023_06_24_180146_create_logs_table.php
+++ database/migrations/2023_06_24_180146_create_logs_table.php
@@ -11,14 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('logs', function (Blueprint $table) {
             $table->id();
+            $table->dateTime('dateTime');
             $table->unsignedInteger('producer_code');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('producer');
+            $table->string('store');
+            $table->string('product');
+            $table->unsignedInteger('price');
+            $table->integer('quantity');
+            $table->integer('store_total')->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('logs');
     }
 };

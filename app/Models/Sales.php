1fb262cc686f05618f75c135da962f3912fc33da
kodaikumatani <koudai.aco5423@gmail.com>
2023-07-18 03:10:02 +0900

    feat: モデルの追加

---
+++ app/Models/Sales.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sales extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'dateTime',
+        'hour',
+        'user_id',
+        'store_id',
+        'product_id',
+        'quantity',
+        'store_total',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'dateTime' => 'datetime',
+        'hour' => 'integer',
+        'user_id' => 'integer',
+        'store_id' => 'integer',
+        'product_id' => 'integer',
+        'quantity' => 'integer',
+        'store_total' => 'integer',
+    ];
+}

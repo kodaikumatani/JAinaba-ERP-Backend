ca0aa360edc721a226f1f9fc8721d4028cbf8f36
kodaikumatani <41698195+kodaikumatani@users.noreply.github.com>
2023-07-24 23:41:22 +0900

    refactor: pintの適用

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -18,7 +17,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'producer_code' => rand(0,1000),
+            'producer_code' => rand(0, 1000),
             'name' => $this->faker->name(),
         ];
     }
